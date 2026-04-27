a524c4e93d68708b7e5a2fdee361c422c65316e8
Nguyễn Dương Minh <nguyenduongminh1112006@gmail.com>
2026-04-27 23:30:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def is_dark():
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
     now_hour = datetime.now().hour
-    return sunset <= now_hour+7 or now_hour+7 <= sunrise
+    return sunset <= now_hour or now_hour <= sunrise
 
 
 if is_dark():
