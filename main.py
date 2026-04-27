7a2c333a92ee5d2fbf194801ce6a9c1c37cace79
Nguyễn Dương Minh <nguyenduongminh1112006@gmail.com>
2026-04-27 22:58:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import smtplib
 
 EMAIL = os.environ.get("MY_GMAIL")
 PASSWORD = os.environ.get("MY_PASSWORD")
-MY_LAT = 21.448250
-MY_LNG = 106.198858
+MY_LAT = -51.1301
+MY_LNG = -5.7778
 
 
 def is_overhead():
